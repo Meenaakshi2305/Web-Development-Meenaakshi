535c683aa9a0252499ee72380b6e4dd63b0d599a
Meenaakshi2305 <2023.meenaakshi.sundarrajan@ves.ac.in>
2024-07-17 21:21:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function fetchWeather(location) {
     fetch(url)
         .then(response => {
             if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
+                throw new Error(`HTTP error! status: `${response.status}`);
             }
             return response.json();
         })
